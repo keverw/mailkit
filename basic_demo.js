e9802f8dc57f009f8e6ec030c47dd6b687489875
Kevin <keverw92@gmail.com>
2012-05-18 13:53:34 -0400

    testing SMTP

--- basic_demo.js
+++ basic_demo.js
@@ -1,16 +1,25 @@
 var mailkit = require('./mailkit.js'); //use just mailkit if installed via NPM
 
+var SMTP = {
+service: "Gmail",
+    auth: {
+        user: "username@gmail.com",
+        pass: "userpass"
+    }
+};
+
 var mailOptions = {
     from_name: "Sender Name",
-    from_addr: "sender@localhost",
+    //from_addr: "sender@localhost",
     to: "lolkatz@localhost", // can also be a list: "receiver1@example.com, receiver2@example.com"
     subject: "Hello ✔", // Subject line,
     encoding: 'html',
     view: './templates/layout.bt',
     subview: './templates/signup.bt',
-    data: {year: '2012'}
+    data: {year: '2012'},
     //html: "<b>Hello world ✔</b>",
-    //body: "<b>Hello world ✔</b>" //body
+    //body: "<b>Hello world ✔</b>" //body,
+    smtp: SMTP
 }
 
 mailkit.send(mailOptions, function(error, status)
