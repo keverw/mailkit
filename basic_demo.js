c99b0fb16b3695e91b10e20426695e1bc0e7929a
Kevin <keverw92@gmail.com>
2012-05-18 01:38:21 -0400

    setting from option works

--- basic_demo.js
+++ basic_demo.js
@@ -1,7 +1,8 @@
 var mailkit = require('./mailkit.js'); //use just mailkit if installed via NPM
 
 var mailOptions = {
-    from: "Sender Name ✔ <sender@localhost>", // sender address
+    from_name: "Sender Name",
+    from_addr: "sender@localhost",
     to: "lolkatz@localhost", // can also be a list: "receiver1@example.com, receiver2@example.com"
     subject: "Hello ✔", // Subject line
     body: "<b>Hello world ✔</b>" //body
--- mailkit.js
+++ mailkit.js
@@ -1,7 +1,31 @@
 (function() {
 
-	function send(options, callback)
+	var nodemailer = require('nodemailer');
+	var transport = null;
+	
+	function send(user_options, callback)
 	{
+		var options = {};
+		
+		//Set the from option
+		if (user_options.from_addr)
+		{
+			if (user_options.from_name)
+			{
+				options.from = user_options.from_name + ' <' + user_options.from_addr + '>';
+			}
+			else
+			{
+				options.from = user_options.from_addr;
+			}
+		}
+		else if (user_options.from)
+		{
+			options.from = user_options.from;
+		}
+		
+		console.log(options);
+		
 		callback(false, {});
 	}
 	// Export public API
